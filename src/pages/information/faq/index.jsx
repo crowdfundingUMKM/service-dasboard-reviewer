f1965ded0faea2df0cdd54bead3d0fb0138fb34c
ahhzaky <siahmadzaky@gmail.com>
2023-11-05 17:48:39 +0700

    Add pages inforamtion /faq /profile

---
+++ src/pages/information/faq/index.jsx
@@ -0,0 +1,191 @@
+import FaqLayout from '@/pages/information/faq/layout';
+import Link from 'next/link';
+
+export default function Faq() {
+    return (
+        <FaqLayout>
+        <main id="main" className="main">
+            {/* start title */}
+            <div className="pagetitle">
+            <h1>F.A.Q Page</h1>
+            <nav>
+                <ol className="breadcrumb">
+                <li className="breadcrumb-item">
+                    <Link href="/dashboard">Home</Link>
+                </li>
+                <li className="breadcrumb-item">Information</li>
+                <li className="breadcrumb-item active">F.A.Q</li>
+                </ol>
+            </nav>
+            </div>
+            {/* End Page Title */}
+            <section className="section">
+            <div className="row">
+                <div className="col-lg">
+                <div className="card">
+                    <div className="card-body">
+                    <h5 className="card-title">Tentang Crowdfunding UMKM</h5>
+                    <p>
+                        Crowdfunding UMKM merupakan platform inovatif yang didedikasikan
+                        untuk mendukung dan mengembangkan Usaha Mikro, Kecil, dan
+                        Menengah (UMKM) di Indonesia. Kami bertujuan untuk memberikan
+                        akses keuangan yang lebih mudah bagi para pelaku usaha UMKM yang
+                        membutuhkan modal untuk mengembangkan bisnis mereka. Dengan
+                        pendekatan crowdfunding, kami mempertemukan para pelaku usaha
+                        dengan para investor yang tertarik untuk berinvestasi pada
+                        proyek-proyek UMKM yang menjanjikan.
+                    </p>
+                    <p>
+                        Dalam Crowdfunding UMKM, para pelaku usaha dapat mengajukan
+                        proposal bisnis mereka yang terperinci dan meyakinkan. Proposal
+                        tersebut mencakup visi, misi, model bisnis, dan rencana
+                        penggunaan modal. Para investor dapat memilih proyek UMKM yang
+                        sesuai dengan minat dan tujuan investasi mereka. Selain
+                        memberikan kesempatan bagi UMKM untuk mendapatkan modal,
+                        Crowdfunding UMKM juga memberdayakan para investor dengan
+                        kesempatan untuk mendapatkan keuntungan dari keberhasilan proyek
+                        UMKM yang mereka dukung. Selain itu, kami juga menyediakan
+                        layanan konsultan yang ahli di bidangnya untuk memberikan
+                        bimbingan dan pengawasan agar proyek-proyek UMKM dapat berjalan
+                        dengan sukses dan memenuhi standar keberlanjutan yang tinggi.
+                        Dengan semangat kolaborasi dan transparansi, Crowdfunding UMKM
+                        berkomitmen untuk menjadi mitra terpercaya dalam pertumbuhan dan
+                        kesuksesan UMKM di Indonesia, serta terus berkontribusi dalam
+                        memajukan sektor UMKM dan ekonomi negara secara keseluruhan.
+                    </p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-lg">
+                <div className="card">
+                    <div className="card-body">
+                    <h5 className="card-title">
+                        Tata Cara Kerja User Reviewer / Konsultan di Crowdfunding UMKM
+                    </h5>
+                    <p>
+                        Tata cara kerja User Reviewer, atau yang juga dikenal sebagai
+                        User Konsultan, di Crowdfunding UMKM sangat penting untuk
+                        memastikan keberhasilan dan integritas dari setiap campaign UMKM
+                        yang diajukan. Berikut adalah langkah-langkah dan poin utama
+                        dalam cara kerja User Reviewer:
+                    </p>
+                    <ul>
+                        <li>
+                        <strong>Analisis Mendalam:</strong> User Reviewer akan
+                        melakukan analisis yang cermat terhadap proposal bisnis yang
+                        diajukan oleh User Campaign. Mereka akan mengevaluasi setiap
+                        aspek proposal, termasuk rencana bisnis, model pendapatan,
+                        dampak sosial, dan rencana keuangan.
+                        </li>
+                        <li>
+                        <strong>Penilaian Objektif:</strong> Setelah analisis, User
+                        Reviewer akan memberikan penilaian objektif terhadap proposal
+                        tersebut. Penilaian ini sering kali mencakup pemberian bintang
+                        atau skor yang mencerminkan sejauh mana proposal tersebut
+                        memenuhi kriteria dan standar yang telah ditetapkan.
+                        </li>
+                        <li>
+                        <strong>Koreksi dan Saran</strong>Jika ditemukan kekurangan
+                        atau perbaikan yang diperlukan dalam proposal, User Reviewer
+                        akan memberikan umpan balik yang konstruktif kepada User
+                        Campaign. Tujuannya adalah membantu User Campaign untuk
+                        meningkatkan kualitas proposal mereka dan membuatnya lebih
+                        menarik bagi investor potensial.
+                        </li>
+                        <li>
+                        <strong>Kepatuhan dengan Ketentuan:</strong>User Reviewer juga
+                        akan memastikan bahwa proposal tersebut mematuhi semua
+                        ketentuan dan regulasi yang berlaku di platform Crowdfunding
+                        UMKM. Mereka bertanggung jawab untuk memastikan tidak ada
+                        pelanggaran atau kecurangan.
+                        </li>
+                        <li>
+                        <strong>Transparansi dan Integritas:</strong>Crowdfunding UMKM
+                        sangat menjunjung tinggi nilai transparansi dan integritas.
+                        Oleh karena itu, User Reviewer harus bertindak secara adil,
+                        jujur, dan tidak memihak dalam melakukan review terhadap
+                        setiap proposal.
+                        </li>
+                        <li>
+                        <strong>Berkolaborasi dengan Admin: </strong>User Reviewer
+                        juga dapat berkolaborasi dengan Admin platform jika terdapat
+                        pertanyaan atau permasalahan yang memerlukan klarifikasi atau
+                        tindakan lebih lanjut.
+                        </li>
+                        <li>
+                        <strong>Feedback Lebih Lanjut:</strong>Selain memberikan
+                        penilaian, User Reviewer juga dapat memberikan komentar lebih
+                        lanjut yang menjelaskan alasan di balik penilaian mereka. Hal
+                        ini dapat membantu User Campaign untuk memahami secara detail
+                        mengenai aspek-aspek yang perlu diperbaiki.
+                        </li>
+                        <li>
+                        <strong>Menyumbangkan Keahlian:</strong> Sebagai ahli di
+                        bidangnya, User Reviewer berperan penting dalam membantu para
+                        pelaku usaha UMKM untuk sukses. Dengan menyumbangkan keahlian
+                        dan pengalaman mereka, mereka turut berkontribusi dalam
+                        pertumbuhan UMKM di Indonesia.
+                        </li>
+                    </ul>
+                    <p>
+                        Tata cara kerja ini memastikan bahwa setiap campaign UMKM yang
+                        ada di Crowdfunding UMKM mendapatkan pengawasan yang cermat dan
+                        konstruktif sehingga dapat meraih dukungan finansial yang lebih
+                        besar dari investor.
+                    </p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-lg">
+                <div className="card">
+                    <div className="card-body">
+                    <h5 className="card-title">
+                        Disclaimer: Crowdfunding UMKM - Platform dalam Tahapan
+                        Pengembangan{" "}
+                    </h5>
+                    <p>
+                        Harap diperhatikan bahwa platform Crowdfunding UMKM yang kami
+                        sajikan saat ini sedang dalam tahapan pengembangan dan hanya
+                        ditujukan untuk penyelesaian kuliah skripsi. Segala fitur,
+                        layanan, dan informasi yang terdapat di dalamnya belum
+                        sepenuhnya beroperasi atau telah diuji secara menyeluruh. Kami
+                        ingin menegaskan bahwa platform ini tidak boleh digunakan untuk
+                        tujuan komersial atau kegiatan bisnis dalam skala penuh, dan
+                        tidak bertanggung jawab atas akibat yang timbul dari penggunaan
+                        yang salah atau penyalahgunaan informasi yang terdapat di
+                        dalamnya.
+                    </p>
+                    <p>
+                        Perlu kami sampaikan bahwa platform ini mengandalkan
+                        implementasi dari 8 service dan 4 frontend yang dibangun
+                        menggunakan teknologi Golang, Gin, Gorm, dan database MySQL,
+                        serta memanfaatkan Next.js sebagai frontend. Meskipun kami telah
+                        berusaha untuk memastikan keamanan dan akurasi informasi, namun
+                        kami tidak dapat menjamin ketepatan dan kesempurnaan data yang
+                        ada pada saat ini.
+                    </p>
+                    <p>
+                        Kami sangat menghargai partisipasi Anda dalam mengeksplorasi
+                        platform ini, namun harap diingat bahwa semua yang terdapat di
+                        dalamnya belum memiliki lisensi untuk digunakan secara komersial
+                        atau sebagaimana ditentukan oleh hukum dan regulasi yang
+                        berlaku. Terima kasih atas pemahaman dan dukungan Anda dalam
+                        menghadapi tahapan pengembangan ini, dan kami berharap dapat
+                        terus memberikan platform yang lebih baik untuk mendukung
+                        pertumbuhan UMKM di masa depan.
+                    </p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </section>
+        </main>
+
+
+        </FaqLayout>
+    );
+}
