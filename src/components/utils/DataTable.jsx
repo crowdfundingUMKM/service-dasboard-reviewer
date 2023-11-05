8a3237df8125a6f87d25abfd33036fecd68ba6e1
ahhzaky <siahmadzaky@gmail.com>
2023-11-05 18:14:31 +0700

    Update DataTable Menu2

--- src/components/utils/DataTable.jsx
+++ src/components/utils/DataTable.jsx
@@ -10,8 +10,10 @@ function DataTableComponent({ children, ...props }) {
   useEffect(() => {
     // Pastikan kita berada di sisi klien (browser)
     if (typeof window !== 'undefined') {
-      const table = new DataTable('.datatable', props);
-      
+      const table = new DataTable('.datatable',{
+        ...props,
+        lengthMenu: [[5, 10, 15, 20, 30, -1],  [ '5', '10', '15', '20', '30', 'Show all' ]] // Set default page length ke 5
+      });
       // Bersihkan DataTable saat komponen dilepas
       return () => {
         if (table) {
